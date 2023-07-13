347f28058d12aa46e1a515281669f09a904eb9c6
Alex Houston <alexehouston@gmail.com>
2023-07-13 12:13:58 -0500

    update css

--- src/Work/Work.jsx
+++ src/Work/Work.jsx
@@ -4,7 +4,43 @@ import transition from "../../transition";
 const Work = () => {
   return (
     <div className="Work animate__animated animate__fadeInUp">
-      <h1>Work</h1>
+      <div className="external">
+        <div className="horizontal-scroll-wrapper">
+          <div className="img-wrapper slower">
+            <img src="/assets/squiz/gameplay.gif" alt="" />
+          </div>
+          <div className="img-wrapper faster">
+              <img src="/assets/eliagal/eg-desktop-home.gif" alt="" />
+          </div>
+          <div className="img-wrapper slower vertical">
+              <img src="/assets/kphaircolor/kp-desktop-home.png" alt="" />
+          </div>
+          <div className="img-wrapper slower slower-down">
+            <img src="/assets/kpxaeh/desktop-home.png" alt="" />
+          </div>
+          <div className="img-wrapper">
+              <img src="/assets/danielsunghoonkim/dsk-desktop-home.gif" alt="" />
+          </div>
+          <div className="img-wrapper slower">
+              <img src="/assets/cineflix/home-screenshot.gif" alt="" />
+          </div>
+          <div className="img-wrapper faster1">
+              <img src="/assets/hamstera/home.png" alt="" />
+          </div>
+          <div className="img-wrapper slower slower2">
+            <img src="/assets/pokebank/home.png" alt="" />
+          </div>
+          <div className="img-wrapper">
+            <img src="/assets/concentration/after-screen.png" alt="" />
+          </div>
+          {/* <div className="img-wrapper slower">
+            <img src="/assets/" alt="" />
+          </div>
+          <div className="img-wrapper slower last">
+            <img src="/assets/" alt="" />
+          </div> */}
+        </div>
+      </div>
     </div>
   );
 };
