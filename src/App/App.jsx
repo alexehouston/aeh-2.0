94e088ad2deae7e1c0fa1da4bc8492fec4e30ae8
ahouston <alexehouston@gmail.com>
2023-08-16 14:47:00 -0500

    update css

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Routes, Route, useLocation } from "react-router-dom";
 import Nav from "../Nav/Nav";
+import Home from "../Home/Home";
 import Resume from "../Resume/Resume";
 import Work from "../Work/Work";
 import About from "../About/About";
@@ -17,6 +18,7 @@ export default function App() {
       <Nav darkMode={darkMode} setDarkMode={setDarkMode} />
 
       <Routes location={location} key={location.pathname}>
+        <Route path="/" index element={<Home />} />
         <Route path="/resume" element={<Resume />} />
         <Route path="/work" element={<Work />} />
         <Route path="/about" element={<About />} />
---
+++ src/Home/Home.jsx
@@ -0,0 +1,9 @@
+import './Home.css';
+
+export default function Home() {
+    return (
+        <div className="Home">
+            <img src="/assets/aeh-warped.png" alt="AEH Warped" />
+        </div>
+    )
+}
