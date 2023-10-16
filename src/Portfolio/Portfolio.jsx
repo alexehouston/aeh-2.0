2c56f053db181b4999ee9573ed2f699f5d49663e
ahouston <alexehouston@gmail.com>
2023-10-16 09:32:29 -0500

    update portfolio

--- src/Portfolio/Portfolio.jsx
+++ src/Portfolio/Portfolio.jsx
@@ -63,6 +63,44 @@ export default function Portfolio() {
         <div className="col-12 col-lg-6">
           <div className="portfolio-item border border-dark bg-white ms-2 d-flex align-items-center">
             <div className="portfolio-img col-6 border-end border-dark">
+              <img src="/assets/cr8tiveelements/cr8tiveelements.gif" alt="" />
+            </div>
+            <div className="item-inner col-12 col-lg-6 px-4">
+              <div className="technology d-flex flex-wrap">
+                <p className="bg-react rounded-pill text-center py-2 pb-1 px-2 mb-2">
+                  React.js
+                </p>
+                <p className="bg-js rounded-pill text-center py-2 pb-1 px-2 ms-1 mb-2">
+                  JavaScript
+                </p>
+              </div>
+              <h5 className="my-4">Cr8tive Elements Hair Studio</h5>
+              <p className="opacity-50">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Voluptas voluptatem, nihil laudantium illum dolorum obcaecati,
+                voluptatibus veniam nesciunt quas minima facilis. Unde porro
+                libero repudiandae.
+              </p>
+              <hr />
+              <div>
+                <a
+                  href="https://github.com/alexehouston/cr8tivehair"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  repo
+                </a>{" "}
+                /{" "}
+                <a href="https://cr8tiveelements.com" target="_blank" rel="noreferrer">
+                  demo
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col-12 col-lg-6">
+          <div className="portfolio-item border border-dark bg-white me-2 mt-3 d-flex align-items-center">
+            <div className="portfolio-img col-6 border-end border-dark">
               <img src="/assets/eliagal/eg-desktop.gif" alt="" />
             </div>
             <div className="item-inner col-12 col-lg-6 px-4">
@@ -99,7 +137,7 @@ export default function Portfolio() {
           </div>
         </div>
         <div className="col-12 col-lg-6">
-          <div className="portfolio-item border border-dark bg-white me-2 mt-3 d-flex align-items-center">
+          <div className="portfolio-item border border-dark bg-white ms-2 mt-3 d-flex align-items-center">
             <div className="portfolio-img col-6 border-end border-dark">
               <img src="/assets/kphaircolor/kphaircolor.gif" alt="" />
             </div>
@@ -141,7 +179,7 @@ export default function Portfolio() {
           </div>
         </div>
         <div className="col-12 col-lg-6">
-          <div className="portfolio-item border border-dark bg-white ms-2 mt-3 d-flex align-items-center">
+          <div className="portfolio-item border border-dark bg-white me-2 mt-3 d-flex align-items-center">
             <div className="portfolio-img col-6 border-end border-dark">
               <img src="/assets/kpxaeh/desktop-home.png" alt="" />
             </div>
@@ -179,7 +217,7 @@ export default function Portfolio() {
           </div>
         </div>
         <div className="col-12 col-lg-6">
-          <div className="portfolio-item border border-dark bg-white me-2 mt-3 d-flex align-items-center">
+          <div className="portfolio-item border border-dark bg-white ms-2 mt-3 d-flex align-items-center">
             <div className="portfolio-img col-6 border-end border-dark">
               <img
                 src="/assets/danielsunghoonkim/dsk-desktop-home.gif"
@@ -224,7 +262,7 @@ export default function Portfolio() {
           </div>
         </div>
         <div className="col-12 col-lg-6">
-          <div className="portfolio-item border border-dark bg-white ms-2 mt-3 d-flex align-items-center">
+          <div className="portfolio-item border border-dark bg-white me-2 mt-3 d-flex align-items-center">
             <div className="portfolio-img col-6 border-end border-dark">
               <img src="/assets/cineflix/home-screenshot.gif" alt="" />
             </div>
@@ -266,7 +304,7 @@ export default function Portfolio() {
           </div>
         </div>
         <div className="col-12 col-lg-6">
-          <div className="portfolio-item border border-dark bg-white me-2 mt-3 d-flex align-items-center">
+          <div className="portfolio-item border border-dark bg-white ms-2 mt-3 d-flex align-items-center">
             <div className="portfolio-img col-6 border-end border-dark">
               <img src="/assets/hamstera/home.png" alt="" />
             </div>
@@ -300,7 +338,7 @@ export default function Portfolio() {
           </div>
         </div>
         <div className="col-12 col-lg-6">
-          <div className="portfolio-item border border-dark bg-white ms-2 mt-3 d-flex align-items-center">
+          <div className="portfolio-item border border-dark bg-white me-2 mt-3 d-flex align-items-center">
             <div className="portfolio-img col-6 border-end border-dark">
               <img src="/assets/pokebank/pokebank.gif" alt="" />
             </div>
@@ -342,7 +380,7 @@ export default function Portfolio() {
           </div>
         </div>
         <div className="col-12 col-lg-6">
-          <div className="portfolio-item border border-dark bg-white me-2 mt-3 d-flex align-items-center">
+          <div className="portfolio-item border border-dark bg-white ms-2 mt-3 d-flex align-items-center">
             <div className="portfolio-img col-6 border-end border-dark">
               <img src="/assets/concentration/concentration.gif" alt="" />
             </div>
