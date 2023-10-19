19625889d3659862855d231754100621d8a8411c
ahouston <alexehouston@gmail.com>
2023-10-19 09:05:00 -0500

    update css

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,4 +1,4 @@
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronDown, faCode } from "@fortawesome/free-solid-svg-icons";
 import Portfolio from "../Portfolio/Portfolio";
@@ -13,23 +13,28 @@ export default function Home({ handleMouseEnter }) {
           <img className="aeh-ai" src="/assets/aeh-ai-2.png" alt="" />
         </div>
         <div className="home-text col-12 col-md-9 col-lg-6 p-0 text-start d-flex flex-column">
-          <h1 className="lh-1">
-            <span className="alex opacity-50">Alex E. Houston</span>
-            <br />
-            Software <FontAwesomeIcon icon={faCode} className="fs-1 opacity-50" />
+          <span className="lh-1 alex opacity-50">Alex E. Houston</span>
+          <h1 className="lh-1 position-relative h-50">
+            Software{" "}
+            <FontAwesomeIcon
+              icon={faCode}
+              className="fa-code fs-1 opacity-50 position-absolute"
+            />
             <br />
             Developer
             <br />
             + Artist
           </h1>
           <p className="intro-text w-75 my-4">
-            I&apos;m a passionate Web Developer based in Houston, Texas. For me, coding is a canvas for art, and I thrive at
-            the intersection of technology and creativity. Explore my portfolio
-            and join me on GitHub as we embark on an exciting journey of
-            innovation.
+            I&apos;m a passionate Web Developer based in Houston, Texas. For me,
+            coding is a canvas for art, and I thrive at the intersection of
+            technology and creativity. Explore my portfolio and join me on
+            GitHub as we embark on an exciting journey of innovation.
           </p>
           <p className="explore-btn col-6 col-md-3 col-lg-3 rounded-pill text-center py-2 mb-0">
-            <a href="#portfolio">Explore <FontAwesomeIcon icon={faChevronDown} /></a>
+            <a href="#portfolio">
+              Explore <FontAwesomeIcon icon={faChevronDown} />
+            </a>
           </p>
         </div>
       </div>
