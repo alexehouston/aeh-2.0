ca03fbd6fea6019fc32b4fd80690ed88a5542583
ahouston <alexehouston@gmail.com>
2024-03-15 12:24:17 -0500

    update css

--- src/Nav/Nav.jsx
+++ src/Nav/Nav.jsx
@@ -23,7 +23,7 @@ export default function Nav({ handleMouseEnter, theme, setTheme }) {
         <div className="container-fluid p-0">
           <div className="col-4 col-lg-10 d-flex align-items-center">
             {theme === 'dark' ? <img className="aeh-img" src="/assets/aeh-small-dark.png" alt="" /> : <img className="aeh-img" src="/assets/aeh-small.png" alt="" />}
-            {theme === 'dark' ? <FontAwesomeIcon className="ms-4 fs-6 align-middle" icon={faSun} onClick={switchTheme} /> : <FontAwesomeIcon className="ms-4 fs-6 align-middle" icon={faMoon} onClick={switchTheme} /> }
+            {theme === 'dark' ? <FontAwesomeIcon className="ms-3 ms-lg-4 fs-6 align-middle" icon={faSun} onClick={switchTheme} /> : <FontAwesomeIcon className="ms-3 ms-lg-4 fs-6 align-middle" icon={faMoon} onClick={switchTheme} /> }
           </div>
           <div className="d-flex col-7 col-lg-2 m-0 d-flex justify-content-end semibold">
             <a href="#portfolio" className="text-end">
